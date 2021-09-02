c2538bd8b990cec15b570c1326da20890caa4550
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-09-02 15:43:52 +0300

    user can add files when updating an answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -16,39 +16,49 @@ feature 'User can edit his answer', "
   end
 
   describe 'Authenticated user', js: true do
-    scenario 'edits his answer' do
-      sign_in user
-      visit question_path(question)
-      click_on 'Edit'
-      within '.answers' do
-        fill_in 'Your answer', with: 'edited answer'
-        click_on 'Save'
-
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited answer'
-        expect(page).to_not have_selector 'textarea'
+    describe 'edits his answer' do
+      background do
+        sign_in user
+        visit question_path(question)
+        click_on 'Edit'
+      end
+      
+      scenario 'with correct parameters' do
+        within '.answers' do
+          fill_in 'Your answer', with: 'edited answer'
+          click_on 'Save'
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
+      
+      scenario 'with errors' do
+        within '.answers' do
+          fill_in 'Your answer', with: ''
+          click_on 'Save'
+          expect(page).to have_content "Body can't be blank"
+          expect(page).to have_content answer.body
+          expect(page).to have_selector 'textarea'
+        end
+      end
+      scenario 'with attached file' do
+        within '.answers' do
+          fill_in 'Your answer', with: 'edited answer'
+          attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on 'Save'
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
       end
     end
+  end
 
-    scenario 'edits his answer with errors' do
-      sign_in user
-      visit question_path(question)
-      click_on 'Edit'
-      within '.answers' do
-        fill_in 'Your answer', with: ''
-        click_on 'Save'
-        expect(page).to have_content "Body can't be blank"
-        expect(page).to have_content answer.body
-        expect(page).to have_selector 'textarea'
-      end
-    end
-
-    scenario "tries to edit other user's question" do
-      sign_in user2
-      visit question_path(question)
-      within '.answers' do
-        expect(page).to_not have_link 'Edit'
-      end
+  scenario "tries to edit other user's question" do
+    sign_in user2
+    visit question_path(question)
+    within '.answers' do
+      expect(page).to_not have_link 'Edit'
     end
   end
 end
