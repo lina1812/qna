51a0f076c938f741e4f1e8e3abf8b266ce5922bd
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-09-02 13:17:31 +0300

    fix after review

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,10 +2,8 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', inverse_of: :author_questions
   belongs_to :best_answer, class_name: 'Answer', optional: true
   has_many :answers, dependent: :destroy
+  has_many :other_answers, ->(question) { where.not(id: question.best_answer_id) }, class_name: 'Answer'
 
   validates :title, :body, presence: true
-
-  def other_answers
-    answers.where.not(id: best_answer_id)
-  end
+  
 end
