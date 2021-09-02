02b494ed3163225d118f8225e46b231a70f42de5
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-09-02 15:23:04 +0300

    user can add files when creating a answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :author, class_name: 'User', inverse_of: :author_answers
   belongs_to :question
+  
+  has_many_attached :files
 
   validates :body, presence: true
 end
