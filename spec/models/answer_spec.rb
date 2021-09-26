2c193babe94640e2de0481d7ea37ad3c1cc69407
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-09-26 12:37:45 +0300

    add shared examples for models

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,11 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
-  it { should have_many(:links).dependent(:destroy) }
-
   it { should validate_presence_of :body }
-
-  it 'have many attached files' do
-    expect(Answer.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  it_behaves_like 'API Model Validation' do
+    let(:klass) { Answer }
   end
 end
