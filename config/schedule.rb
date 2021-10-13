f84857821abcfb9f7f70d158e41f22e6e3bb5cf1
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-10-13 11:52:05 +0300

    config for mailers

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,8 +13,11 @@
 #   rake "some:great:rake:task"
 # end
 #
-every 5.minutes do
+every 1.day do
   runner 'DailyDigestJob.perform_now'
 end
 
+every 30.minutes do
+  rake "ts:index"
+end
 # Learn more: http://github.com/javan/whenever
