18ea19e10019e6e4d28a4045761cca8ac784f6b8
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-10-27 22:36:17 +0300

    add cache

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
   include Votable
   belongs_to :author, class_name: 'User', inverse_of: :author_answers
-  belongs_to :question
+  belongs_to :question, touch: true
 
   has_many :links, dependent: :destroy, as: :linkable
   has_many :comments, dependent: :destroy, as: :commentable
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', inverse_of: :author_votes
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
 
   validates :body, presence: true
 end
